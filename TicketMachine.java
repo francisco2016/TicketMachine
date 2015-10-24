053901a0de73273509bfe4c44e4c71df8212fd14
francisco2015 <otrayama@gmail.com>
2015-10-24 12:35:20 +0200

    Creado método showPrice, para mostrar por pantalla el precio del billete.

--- TicketMachine.java
+++ TicketMachine.java
@@ -76,11 +76,13 @@ public class TicketMachine
         balance = 0;
     }
     
+    
+    
     /***
      * Método para mostrar por pantalla..
      */
-    public void prompt(){
-        System.out.println("Please, insert the correct amount of money.");
+    public void showPrice(){
+        System.out.println("The price of a ticket is" + price + "cents.");
     
     }
     
